c38fa559727597575112d600e7fa506dd8862ba8
scavvv13 <scave1013@gmail.com>
2024-08-31 14:25:06 +0800

    updated cors

--- api/index.js
+++ api/index.js
@@ -45,7 +45,6 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use(
   cors({
     origin: ["https://document-management-system-liard.vercel.app"], // Allow your Vercel domain
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
   })
 );
