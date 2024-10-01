d6689c1b8035d14a53e9ac2ca65d82d5c48f404f
scavvv13 <scave1013@gmail.com>
2024-10-01 20:52:40 +0800

    edited api end

--- api/index.js
+++ api/index.js
@@ -900,14 +900,14 @@ app.delete("/notifications/:id", async (req, res) => {
 
 // Add this route to your existing routes
 
-app.options("/api/documents/:documentId/share", (req, res) => {
+app.options("documents/:documentId/share", (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://document-management-system-1-0b91.onrender.com');
   res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.sendStatus(200); // Send a successful response for preflight requests
 });
 
-app.post("/api/documents/:documentId/share", async (req, res) => {
+app.post("documents/:documentId/share", async (req, res) => {
   const { documentId } = req.params;
   const { email } = req.body;
 
